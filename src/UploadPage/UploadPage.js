ad66ae48b2c9feb9bcbeda72d8ee075a0ab423f4
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-03-16 08:27:53 -0700

    Added AWS SDK and fiddled with S3 uploads.

--- src/UploadPage/UploadPage.js
+++ src/UploadPage/UploadPage.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Dropzone from 'react-dropzone-uploader';
+import S3 from 'aws-sdk/clients/s3';
 import 'react-dropzone-uploader/dist/styles.css';
 
-import { userService } from '../_services';
-
 class Uploader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bucketName: 's3-dropzone-test',
+      s3: new S3({
+        apiVersion: '2006-03-01',
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      })
+    };
+  }
+
   render() {
-    const getUploadParams = () => {
-      return { url: 'https://httpbin.org/post' }
+    const getUploadParams = ({ meta: {name} }) => {
+      let params = {
+        Bucket: this.state.bucketName,
+        Fields: {key: name}
+      };
+      this.state.s3.createPresignedPost(params, function(err, data) {
+        if (err) {
+          console.error("Presigning post data encountered an error: ", err);
+          return '';
+        }
+        else {
+          console.log("The POST data is ", data);
+          let {fields, url} = data;
+          return {fields, meta: {name}, url: url}
+        }
+      })
     }
 
     const handleChangeStatus = ({ meta }, status) => {
@@ -32,48 +57,32 @@ class Uploader extends React.Component {
 }
 
 class UploadPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            user: {},
-            users: []
-        };
-    }
+    this.state = {
+      user: {}
+    };
+  }
 
-    componentDidMount() {
-        this.setState({
-            user: JSON.parse(localStorage.getItem('user')),
-            users: { loading: true }
-        });
-        userService.getAll().then(users => this.setState({ users }));
-    }
+  componentDidMount() {
+    this.setState({
+      user: JSON.parse(localStorage.getItem('user'))
+    });
+  }
 
-    render() {
-        const { user, users } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h1>Greetings, {user.firstName}!</h1>
-                <Uploader />
-{/*
-                <h3>Users from secure api end point:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.length &&
-                    <ul>
-                        {users.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                            </li>
-                        )}
-                    </ul>
-                }
-*/}
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
-            </div>
-        );
-    }
+  render() {
+    const { user } = this.state;
+    return (
+      <div className="col-md-6 col-md-offset-3">
+        <h1>Greetings, {user.firstName}!</h1>
+        <Uploader />
+        <p>
+          <Link to="/login">Logout</Link>
+        </p>
+      </div>
+    );
+  }
 }
 
 export { UploadPage };
